c9f235804c2b3c7d909f1aebd5715429a63f07c0
Igor <g0kanik@yandex.ru>
2025-06-24 12:15:24 +0300

    update emotion_class paths

--- scripts/emotion_class.py
+++ scripts/emotion_class.py
@@ -1,13 +1,20 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import torch.nn.functional as F
+import os
 
 class Emotion_detector():
 
     def __init__(self):
-        local_model_path = "D:/project/Python25/ruBert_emotion_model"
+        # Получаем путь к директории, где находится этот файл
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        # Собираем относительный путь к папке с моделью
+        local_model_path = os.path.join(base_dir, "../ruBert_emotion_model")
+        # Приводим к нормальной форме (убирает лишние ../)
+        local_model_path = os.path.normpath(local_model_path)
         self.tokenizer = AutoTokenizer.from_pretrained(local_model_path)
         self.model = AutoModelForSequenceClassification.from_pretrained(local_model_path)
+
 
     def start(self, text:str):
         inputs = self.tokenizer(text, return_tensors="pt", truncation=True, padding=True)
